ab354ff57e304e8004d939e9a22c109a9c3f4aae
cyan3 <cyan@bluespace.ai>
2020-07-20 21:13:46 -0700

    #cleanup formatting

--- pupper/HardwareInterface.py
+++ pupper/HardwareInterface.py
@@ -1,23 +1,20 @@
 import pigpio
 from pupper.Config import ServoParams, PWMParams
 
-
-class HardwareInterface:
-    def __init__(self):
-        self.pi = pigpio.pi()
-        self.pwm_params = PWMParams()
-        self.servo_params = ServoParams()
-        initialize_pwm(self.pi, self.pwm_params)
-
-    def set_actuator_postions(self, joint_angles):
-        send_servo_commands(self.pi, self.pwm_params, self.servo_params, joint_angles)
-    
-    def set_actuator_position(self, joint_angle, axis, leg):
-        send_servo_command(self.pi, self.pwm_params, self.servo_params, joint_angle, axis, leg)
-
+def initialize_pwm(pi, pwm_params):
+    for leg_index in range(4):
+        for axis_index in range(3):
+            pi.set_PWM_frequency(
+                pwm_params.pins[axis_index, leg_index],
+                pwm_params.freq
+            )
+            pi.set_PWM_range(
+                pwm_params.pins[axis_index, leg_index],
+                pwm_params.range)
 
 def pwm_to_duty_cycle(pulsewidth_micros, pwm_params):
-    """Converts a pwm signal (measured in microseconds) to a corresponding duty cycle on the gpio pwm pin
+    """Converts a pwm signal (measured in microseconds)
+    to a corresponding duty cycle on the gpio pwm pin
 
     Parameters
     ----------
@@ -33,7 +30,6 @@ def pwm_to_duty_cycle(pulsewidth_micros, pwm_params):
     """
     return int(pulsewidth_micros / 1e6 * pwm_params.freq * pwm_params.range)
 
-
 def angle_to_pwm(angle, servo_params, axis_index, leg_index):
     """Converts a desired servo angle into the corresponding PWM command
 
@@ -44,9 +40,16 @@ def angle_to_pwm(angle, servo_params, axis_index, leg_index):
     servo_params : ServoParams
         ServoParams object
     axis_index : int
-        Specifies which joint of leg to control. 0 is abduction servo, 1 is inner hip servo, 2 is outer hip servo.
+        Specifies which joint of leg to control.
+        0 is abduction servo,
+        1 is inner hip servo,
+        2 is outer hip servo.
     leg_index : int
-        Specifies which leg to control. 0 is front-right, 1 is front-left, 2 is back-right, 3 is back-left.
+        Specifies which leg to control.
+        0 is front-right,
+        1 is front-left,
+        2 is back-right,
+        3 is back-left.
 
     Returns
     -------
@@ -62,21 +65,11 @@ def angle_to_pwm(angle, servo_params, axis_index, leg_index):
     )
     return pulse_width_micros
 
-
 def angle_to_duty_cycle(angle, pwm_params, servo_params, axis_index, leg_index):
     return pwm_to_duty_cycle(
-        angle_to_pwm(angle, servo_params, axis_index, leg_index), pwm_params
+        angle_to_pwm(angle, servo_params, axis_index, leg_index),
+        pwm_params
     )
-
-
-def initialize_pwm(pi, pwm_params):
-    for leg_index in range(4):
-        for axis_index in range(3):
-            pi.set_PWM_frequency(
-                pwm_params.pins[axis_index, leg_index], pwm_params.freq
-            )
-            pi.set_PWM_range(pwm_params.pins[axis_index, leg_index], pwm_params.range)
-
 
 def send_servo_commands(pi, pwm_params, servo_params, joint_angles):
     for leg_index in range(4):
@@ -88,15 +81,48 @@ def send_servo_commands(pi, pwm_params, servo_params, joint_angles):
                 axis_index,
                 leg_index,
             )
-            pi.set_PWM_dutycycle(pwm_params.pins[axis_index, leg_index], duty_cycle)
-
+            pi.set_PWM_dutycycle(
+                pwm_params.pins[axis_index, leg_index],
+                duty_cycle)
 
 def send_servo_command(pi, pwm_params, servo_params, joint_angle, axis, leg):
-    duty_cycle = angle_to_duty_cycle(joint_angle, pwm_params, servo_params, axis, leg)
-    pi.set_PWM_dutycycle(pwm_params.pins[axis, leg], duty_cycle)
+    duty_cycle = angle_to_duty_cycle(
+        joint_angle,
+        pwm_params,
+        servo_params,
+        axis,
+        leg)
 
+    pi.set_PWM_dutycycle(
+        pwm_params.pins[axis, leg],
+        duty_cycle)
 
 def deactivate_servos(pi, pwm_params):
     for leg_index in range(4):
         for axis_index in range(3):
-            pi.set_PWM_dutycycle(pwm_params.pins[axis_index, leg_index], 0)
+            pi.set_PWM_dutycycle(
+                pwm_params.pins[axis_index, leg_index],
+                0)
+
+class HardwareInterface:
+    def __init__(self):
+        self.pi = pigpio.pi()
+        self.pwm_params = PWMParams()
+        self.servo_params = ServoParams()
+        initialize_pwm(self.pi, self.pwm_params)
+
+    def set_actuator_postions(self, joint_angles):
+        send_servo_commands(
+            self.pi,
+            self.pwm_params,
+            self.servo_params,
+            joint_angles)
+    
+    def set_actuator_position(self, joint_angle, axis, leg):
+        send_servo_command(
+            self.pi,
+            self.pwm_params,
+            self.servo_params,
+            joint_angle,
+            axis,
+            leg)
--- src/JoystickInterface.py
+++ src/JoystickInterface.py
@@ -5,11 +5,11 @@ from src.State import BehaviorState, State
 from src.Command import Command
 from src.Utilities import deadband, clipped_first_order_filter
 
-
 class JoystickInterface:
-    def __init__(
-        self, config, udp_port=8830, udp_publisher_port = 8840,
-    ):
+    def __init__(self,
+        config,
+        udp_port=8830,
+        udp_publisher_port = 8840):
         self.config = config
         self.previous_gait_toggle = 0
         self.previous_state = BehaviorState.REST
@@ -17,26 +17,41 @@ class JoystickInterface:
         self.previous_activate_toggle = 0
 
         self.message_rate = 50
-        self.udp_handle = UDPComms.Subscriber(udp_port, timeout=0.3)
-        self.udp_publisher = UDPComms.Publisher(udp_publisher_port)
+        self.udp_handle = UDPComms.Subscriber(
+            udp_port, timeout=0.3)
+        self.udp_publisher = UDPComms.Publisher(
+            udp_publisher_port)
 
-
-    def get_command(self, state, do_print=False):
+    def get_command(self,
+        state,
+        do_print=False):
         try:
+            '''
+            consume UDP message, produce command
+            R1: rest to trot, raise trot_event
+            x: rest to hop, raise hop_event
+            L1: rest to activate, raise activate_event
+            
+            '''
             msg = self.udp_handle.get()
             command = Command()
             
             ####### Handle discrete commands ########
-            # Check if requesting a state transition to trotting, or from trotting to resting
+            # Check if requesting a state transition
+            # to trotting, or from trotting to resting
             gait_toggle = msg["R1"]
-            command.trot_event = (gait_toggle == 1 and self.previous_gait_toggle == 0)
+            command.trot_event = (
+                gait_toggle == 1 and self.previous_gait_toggle == 0)
 
-            # Check if requesting a state transition to hopping, from trotting or resting
+            # Check if requesting a state transition
+            # to hopping, from trotting or resting
             hop_toggle = msg["x"]
-            command.hop_event = (hop_toggle == 1 and self.previous_hop_toggle == 0)            
+            command.hop_event = (
+                hop_toggle == 1 and self.previous_hop_toggle == 0)            
             
             activate_toggle = msg["L1"]
-            command.activate_event = (activate_toggle == 1 and self.previous_activate_toggle == 0)
+            command.activate_event = (
+                activate_toggle == 1 and self.previous_activate_toggle == 0)
 
             # Update previous values for toggles and state
             self.previous_gait_toggle = gait_toggle
@@ -65,10 +80,12 @@ class JoystickInterface:
             command.pitch = state.pitch + message_dt * pitch_rate
 
             height_movement = msg["dpady"]
-            command.height = state.height - message_dt * self.config.z_speed * height_movement
+            command.height = state.height -\
+                message_dt * self.config.z_speed * height_movement
             
             roll_movement = - msg["dpadx"]
-            command.roll = state.roll + message_dt * self.config.roll_speed * roll_movement
+            command.roll = state.roll +\
+                message_dt * self.config.roll_speed * roll_movement
 
             return command
 
@@ -76,7 +93,6 @@ class JoystickInterface:
             if do_print:
                 print("UDP Timed out")
             return Command()
-
 
     def set_color(self, color):
         joystick_msg = {"ps4_color": color}
