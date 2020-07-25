258e4a5b3d7d271df988f381be8b8c703c4622ca
cyan3 <cyan@bluespace.ai>
2020-07-24 23:30:27 -0700

    refactor and flesh out controller_shakeout

--- controller_shakeout.py
+++ controller_shakeout.py
@@ -21,9 +21,91 @@ from pupper.HardwareInterface import HardwareInterface
 from pupper.Config import Configuration
 from pupper.Kinematics import four_legs_inverse_kinematics
 
-# interaction
+# command
+from gamepad import *
+from src.Command import Command
+from src.JoystickInterface import JoystickInterface
 
-import argparse
+class CommandGamepad(Gamepad):
+  def __init__(self):
+    config = Configuration()
+    self.joystick_interface = JoystickInterface(config)
+
+    # joystickinterface expects a state
+    # whereas gamepad captures deltas
+    self._msg = {
+      # discrete between 0 (off) and 1 (on)
+      "R1": 0,
+      "x": 0,
+      "L1": 0,
+
+      # continuous these all go between 0.0 and 1.0
+      "ly": 0.0,
+      "lx": 0.0,
+      "rx": 0.0,
+      "ry": 0.0,
+      "dpady": 0.0,
+      "dpadx": 0.0,
+
+      # other
+      "message_rate": 20, # 20 Hz
+
+      # # unused
+      # "L2": L2,
+      # "R2": R2,
+      # "square": square,
+      # "circle": circle,
+      # "triangle": triangle,
+    }
+
+    self._cmd_target_name = None
+
+    super(CommandGamepad, self).__init__()
+
+  def init_external_blackboard(self, *args):
+    # initialize / forward-declare
+    # what pupper_ed needs
+
+    if type(args[0]) is not dict:
+      raise Exception("CommandGamepad expects dict, str")
+    if type(args[1]) is not str:
+      raise Exception("CommandGamepad expects dict, str")
+
+    self.external_blackboard = external_blackboard
+
+    self._cmd_target_name = args[1]
+
+    mutex_name = self._cmd_target_name + "_mutex"
+    if mutex_name not in self.external_blackboard:
+      # without creating one explicitly
+      # condition has underlying mutex
+      self.external_blackboard[mutex_name] = Lock()
+
+    cv_name = self._cmd_target_name + "_cv"
+    if cv_name not in self.external_blackboard:
+      self.external_blackboard[cv_name] = Condition(
+        self.external_blackboard[mutex_name])
+
+    queue_name = self._cmd_target_name + "_queue"
+    if queue_name not in self.external_blackboard:
+      self.external_blackboard[queue_name] = []
+
+  def produce(self, k, v):
+    # produce a command to cmd_target
+
+    self._msg[k] = v
+    cmd = self.joystickinterface.build_command(
+      self._msg)
+
+    event_dispatch.blackboard[self._cmd_target_name + "_cv"].acquire()
+    event_dispatch.blackboard[self._cmd_target_name + "_queue"].append(
+      [
+        "CmdSetEvent",
+        1,
+        self._cmd_target_name,
+        self._cmd_target_name])
+    event_dispatch.blackboard[self._cmd_target_name + "_cv"].notify(1)
+    event_dispatch.blackboard[self._cmd_target_name + "_cv"].release()
 
 class Pupper(IterableObject):
   def __init__(self):
@@ -39,6 +121,8 @@ class Pupper(IterableObject):
 
     self._blackboard = None
 
+    self._cmd = Command()
+
   def do_init(self, *args, **kwargs):
     self.controller.set_pose_to_default(self.state)
     self.hardware_interface.set_actuator_postions(
@@ -47,97 +131,24 @@ class Pupper(IterableObject):
     if type(args[0]) is not dict:
       raise Exception(
         "expected init with blackboard")
-
     self._blackboard = args[0]
-
-    if "pupper_command" not in self._blackboard:
-      raise Exception(
-        "expected init with pupper_command")
 
   def do_iterate(self, *args, **kwargs):
     self.controller.run(
       self.state,
-      self._blackboard["pupper_command"])
+      self._cmd)
 
   def do_cleanup(self):
     pass
 
-class PupperRunEvent(Event):
-  def __init__(self, event_id, *args, **kwargs):
-    super(PupperRunEvent, self).__init__(
-      event_id, *args, **kwargs)
-
+class CmdSetEvent(IterateEvent):
   def dispatch(self, event_dispatch, *args, **kwargs):
-    super(PupperRunEvent, self).dispatch(
-      event_dispatch, *args, **kwargs)
-
-    # print("dispatching")
+    # set the pupper's cmd
     event_dispatch.blackboard[
-      "pupper"].iterate(event_dispatch)
-    # time.sleep(0.05)
-    # print("dispatch ending")
+      "pupper"]._cmd = args[2]
 
-  def finish(self, event_dispatch, *args, **kwargs):
-    # ############### option A:
-    # spawn the next event as an explicit
-    # **child** thread
-    # constructor_args = (self.event_id,
-    #   event_dispatch.blackboard)
-    # event_dispatch.dispatch(
-    #   blackboard["ControllerCheckEvent"](
-    #     *constructor_args), ())
-
-    # ############### option B:
-    # spawn the next event through the queue
-    # and the ED, aka, a **sibling** thread
-
-    event_dispatch.blackboard["pupper_cv"].acquire()
-    event_dispatch.blackboard["pupper_queue"].append(
-      ["PupperRunEvent", self.event_id])
-    event_dispatch.blackboard["pupper_cv"].notify(1)
-    event_dispatch.blackboard["pupper_cv"].release()
-
-    # event_dispatch.blackboard["controller_queue"].pop(0)
-
-  @staticmethod
-  def deserialize(blackboard, *args, **kwargs):
-    tokens = args[0] # args is a tuple of 1 list, that list is tokens
-    if len(tokens) != 1:
-      raise Exception("expected 1 token")
-    return (int(tokens[0]), blackboard), () # tuple
-
-class InputCheckEvent(Event):
-  def __init__(self, event_id, *args, **kwargs):
-    super(InputCheckEvent, self).__init__(
-      event_id, *args, **kwargs)
-
-  def dispatch(self, event_dispatch, *args, **kwargs):
-    super(InputCheckEvent, self).dispatch(
+    super(CmdSetEvent, self).dispatch(
       event_dispatch, *args, **kwargs)
-
-    command_input = str(input("Enter command"))
-
-    event_dispatch.blackboard["pupper_command"]
-
-  def finish(self, event_dispatch, *args, **kwargs):
-    # ############### option A:
-    # spawn the next event as an explicit
-    # **child** thread
-    # constructor_args = (self.event_id,
-    #   event_dispatch.blackboard)
-    # event_dispatch.dispatch(
-    #   blackboard["ControllerCheckEvent"](
-    #     *constructor_args), ())
-
-    # ############### option B:
-    # spawn the next event through the queue
-    # and the ED, aka, a **sibling** thread
-
-    event_dispatch.blackboard["input_cv"].acquire()
-    event_dispatch.blackboard["input_queue"].append(
-      ["InputCheckEvent", self.event_id])
-    event_dispatch.blackboard["input_cv"].notify(1)
-    event_dispatch.blackboard["input_cv"].release()
 
   @staticmethod
   def deserialize(blackboard, *args, **kwargs):
@@ -147,18 +158,13 @@ class InputCheckEvent(Event):
     return (int(tokens[0]), blackboard), () # tuple
 
 if __name__ == "__main__":
+  from common import bcolors, nonempty_queue_exists
+  from common import IterateEvent, BlackboardQueueCVED
+
+  import argparse
+  from threading import Lock, Condition, Thread
+
   parser = argparse.ArgumentParser()
-  # parser.add_argument(
-  #   "--leg", type=int, help="leg",
-  #   default=0)
-  # parser.add_argument(
-  #   "--axis", type=int,
-  #   help="axis index: 0 abduct, 1 inner hip, 2 outer hip",
-  #   default=0)
-  # parser.add_argument(
-  #   "--delta", type=float,
-  #   help="delta in degs to move, clamped",
-  #   default=5.0)
   args = parser.parse_args()
 
   ############### overhead
@@ -174,8 +180,14 @@ if __name__ == "__main__":
   blackboard["done_queue"] = [1]
 
   ############### actors
+  gamepad = CommandGamepad()
+  gamepad.init(blackboard, "pupper")
+  if not gamepad.initialized():
+    print("couldn't initialize gamepad")
+    sys.exit(1)
+  blackboard["gamepad"] = gamepad
+
   pupper = Pupper()
-  blackboard["pupper_command"] = Command()
   pupper.init(blackboard)
   if not pupper.initialized():
     print("couldn't initialize pupper")
@@ -183,6 +195,16 @@ if __name__ == "__main__":
   blackboard["pupper"] = pupper
 
   ############### dispatches
+  gamepad_ed = BlackboardQueueCVED(
+    blackboard, "gamepad")
+  blackboard["gamepad_thread"] = Thread(
+    target=gamepad_ed.run,
+    args=(blackboard,
+      "gamepad",
+      # "done",
+      None,
+      bcolors.CYAN))
+
   pupper_ed = BlackboardQueueCVED(
     blackboard, "pupper")
   blackboard["pupper_thread"] = Thread(
@@ -191,38 +213,28 @@ if __name__ == "__main__":
       "pupper",
       # "done",
       None,
-      bcolors.CYAN))
-
-  input_ed = BlackboardQueueCVED(
-    blackboard, "input")
-  blackboard["input_thread"] = Thread(
-    target=input_ed.run,
-    args=(blackboard,
-      "input",
-      # "done",
-      None,
       bcolors.GREEN))
 
   ############### events
-  blackboard["PupperRunEvent"] = PupperRunEvent
-  blackboard["InputCheckEvent"] = InputCheckEvent
+  blackboard["IterateEvent"] = IterateEvent
+  blackboard["CmdSetEvent"] = CmdSetEvent
 
   ############### process init
+  blackboard["gamepad_cv"].acquire()
+  blackboard["gamepad_queue"].append(
+    ["IterateEvent", 1, "gamepad", "gamepad"])
+  blackboard["gamepad_cv"].notify(1)
+  blackboard["gamepad_cv"].release()
+
   blackboard["pupper_cv"].acquire()
   blackboard["pupper_queue"].append(
-    ["PupperRunEvent", 1])
+    ["IterateEvent", 1, "pupper", "pupper"])
   blackboard["pupper_cv"].notify(1)
   blackboard["pupper_cv"].release()
 
-  blackboard["input_cv"].acquire()
-  blackboard["input_queue"].append(
-    ["InputCheckEvent", 1])
-  blackboard["input_cv"].notify(1)
-  blackboard["input_cv"].release()
-
   ############### process lifecycle
+  blackboard["gamepad_thread"].start()
   blackboard["pupper_thread"].start()
-  blackboard["input_thread"].start()
 
   ### Shutdown procedure
   print("### SHUT DOWN ###")
@@ -252,4 +264,5 @@ if __name__ == "__main__":
     print("joining", k)
     blackboard[k].join()
 
+  gamepad.cleanup()
   pupper.cleanup()
